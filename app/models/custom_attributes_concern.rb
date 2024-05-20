f3957c3fb1bd796dd3369e22433ba7e6c34ba481
Donna Mwiine <dmwiine@gmail.com>
2024-05-20 15:02:17 +0300

    some refactoring

--- app/models/custom_attributes_concern.rb
+++ app/models/custom_attributes_concern.rb
@@ -6,21 +6,21 @@ module CustomAttributesConcern
   end
 
   # Method to get custom attribute value
-  def get_custom_attribute(attribute_name)
-    custom_attribute = custom_attributes.find_by(name: attribute_name)
+  def get_custom_attribute(attribute_name, object)
+    custom_attribute = custom_attributes.find_by(name: attribute_name, attributable_type: object.class.to_s, attributable_id: object.id)
     custom_attribute.value if custom_attribute
   end
 
   # Method to set custom attribute value
-  def set_custom_attribute(attribute_name, value)
-    custom_attribute = custom_attributes.find_or_initialize_by(name: attribute_name)
+  def set_custom_attribute(attribute_name, value, object)
+    custom_attribute = custom_attributes.find_or_initialize_by(name: attribute_name, attributable_type: object.class.to_s, attributable_id: object.id)
     custom_attribute.value = value
     custom_attribute.save
   end
 
   # Method to delete custom attribute
-  def delete_custom_attribute(attribute_name)
-    custom_attributes.find_by(name: attribute_name)&.destroy
+  def delete_custom_attribute(attribute_name, object)
+    custom_attributes.find_by(name: attribute_name, attributable_type: object.class.to_s, attributable_id: object.id)&.destroy
   end
   
 end
--- test/models/custom_attributes_concern_test.rb
+++ test/models/custom_attributes_concern_test.rb
@@ -3,49 +3,49 @@ require 'test_helper'
 
 class CustomAttributesConcernTest < ActiveSupport::TestCase
   def setup
-    @customer = DummyCustomer.create(name: 'Test Customer')
+    @customer = Customer.create(name: 'Test Customer')
 
   end
 
   def test_concern_inclusion
-    assert_includes DummyCustomer.included_modules, CustomAttributesConcern
+    assert_includes Customer.included_modules, CustomAttributesConcern
   end
 
   def test_association_with_custom_attributes
-    association = DummyCustomer.reflect_on_association(:custom_attributes)
+    association = Customer.reflect_on_association(:custom_attributes)
     
     assert_equal :has_many, association.macro
     assert_equal :attributable, association.options[:as]
   end
 
   def test_get_custom_attribute_returns_value
-    @customer.set_custom_attribute('email', 'test@example.com')
-    assert_equal 'test@example.com', @customer.get_custom_attribute('email')
+    @customer.set_custom_attribute('email', 'test@example.com', @customer)
+    assert_equal 'test@example.com', @customer.get_custom_attribute('email', @customer)
   end
 
   def test_get_custom_attribute_returns_nil_for_non_existent_attribute
-    assert_nil @customer.get_custom_attribute('phone')
+    assert_nil @customer.get_custom_attribute('phone', @customer)
   end
 
   def test_set_custom_attribute_creates_new_attribute
-    @customer.set_custom_attribute('email', 'test@example.com')
+    @customer.set_custom_attribute('email', 'test@example.com', @customer)
     assert_equal 1, @customer.custom_attributes.length
   end
 
   def test_set_custom_attribute_updates_existing_attribute_value
-    @customer.set_custom_attribute('email', 'test@example.com')
-    @customer.set_custom_attribute('email', 'updated@example.com')
+    @customer.set_custom_attribute('email', 'test@example.com', @customer)
+    @customer.set_custom_attribute('email', 'updated@example.com', @customer)
     assert_equal 'updated@example.com', @customer.custom_attributes.find_by(name: 'email').value
   end
 
   def test_delete_custom_attribute_removes_attribute
-    @customer.set_custom_attribute('email', 'test@example.com')
-    @customer.delete_custom_attribute('email')
+    @customer.set_custom_attribute('email', 'test@example.com', @customer)
+    @customer.delete_custom_attribute('email', @customer)
     assert_equal 0, @customer.custom_attributes.length
   end
 end
 
-class DummyCustomer < ActiveRecord::Base
+class Customer < ActiveRecord::Base
     self.table_name = 'customers'
   
     include CustomAttributesConcern
